682f011c81e0ea970708fcc859113d12525e6872
George Arakelian <88378044+geoking1907@users.noreply.github.com>
2022-04-19 13:40:19 +0300

    Blogs added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/routes')
+const bp = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 
@@ -10,6 +11,9 @@ const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: '.hbs'
 })
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,120 @@
-const {Router} = require('express')
+const {Router} = require('express');
 
-const router = Router()
+const sqlite3 = require('sqlite3').verbose();
+let sql;
 
+const router = Router();
+
+
+const cgdb = new sqlite3.Database("cg.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err.message);
+});
+const drdb = new sqlite3.Database("dr.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err.message);
+});
+
+/*
+drdb.run('CREATE TABLE IF NOT EXISTS doshirak(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255) NOT NULL, short VARCHAR(255) NOT NULL, text TEXT)');
+*/
 
 router.get('/', function(req, res) {
   res.render('index', {
     title: 'Аракелян Георгий'
-  })
+  });
 });
+
+router.get('/admin/', async (req, res) => {
+  res.render('admin', {
+    title: 'Administrator'
+  });
+});
+
+// cryptography
+
+router.get('/cryptography/', async (req, res) => {
+  sql = `SELECT * FROM cryptography`;
+  cgdb.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+      console.log(row)
+    })
+    res.render('cryptography', {
+      title: 'Шифры',
+      rows: rows
+    });
+  });
+});
+
+router.get('/cryptography-add/', async (req, res) => {
+  sql = `SELECT * FROM cryptography`;
+  cgdb.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+      console.log(row)
+    })
+    res.render('cradd', {
+      title: 'CrAddPanel',
+      rows: rows
+    });
+  });
+});
+
+router.post('/cradd', async (req, res) => {
+    cgdb.run(
+      'INSERT INTO cryptography (title, short, text) VALUES (?,?,?)', 
+      [req.body.title, req.body.short, req.body.text], 
+      (err) => {
+        if (err) return console.error(err.message);
+      });
+      res.redirect('/cryptography-add')
+});
+
+// doshirak
+
+router.get('/doshirak/', async (req, res) => {
+  sql = `SELECT * FROM doshirak`;
+  drdb.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+      console.log(row)
+    })
+    res.render('sd-doshirak', {
+      title: 'Дошики',
+      rows: rows
+    });
+  });
+});
+
+router.get('/doshirak-add/', async (req, res) => {
+  sql = `SELECT * FROM doshirak`;
+  drdb.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+      console.log(row)
+    })
+    res.render('sd-dradd', {
+      title: 'DrAddPanel',
+      rows: rows
+    });
+  });
+});
+
+router.post('/dradd', async (req, res) => {
+    drdb.run(
+      'INSERT INTO doshirak (title, short, text) VALUES (?,?,?)', 
+      [req.body.title, req.body.short, req.body.text], 
+      (err) => {
+        if (err) return console.error(err.message);
+      });
+      res.redirect('/doshirak-add')
+});
+
 
 router.use((req, res, next) => {
   res.status(404).render('error', {
     title: 'Где мы?!'
-  })
-})
+  });
+});
 
-module.exports = router
+
+module.exports = router;
